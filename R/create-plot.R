460fd374ff77c9602ca3d09f254d66f1eb6b1139
UniCandice <liyuan.liu@newcastle.ac.uk>
2025-10-28 14:17:52 +0000

    include the images

--- R/create-plot.R
+++ R/create-plot.R
@@ -1,24 +1,22 @@
 # Simple script analysing the Australian weather data
 
-library(tidyverse)
+library("tidyverse")
 
 # Read in the data
-df <- read_csv("data/weatherAUS.csv")
 
-# Choose a city
-city <- "Melbourne and Brisbane"
+df = read_csv("data/weatherAUS.csv")
 
 # Create a scatter plot of Humidity9am and Temp9am
-scatter_plot <- df %>%
-  filter(Location == city) %>%
-  ggplot(aes(x = Temp9am, y = Humidity9am)) +
-  geom_point() +
-  labs(
-    title = paste("Temperature vs Humidity at 9am in", city),
-    x = "Temperature at 9am (°C)",
-    y = "Humidity at 9am (%)"
-  )
+# a location
 
-# Save the plot to graphics/
-ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot, width = 6, height = 4, dpi = 300)
+city = "Brisbane"
+
+scatter_plot = 
+  df %>%
+  filter(Location == city) %>%
+  ggplot(aes(x = Temp9am, y = Humidity9am)) + 
+  geom_point()
+
+# Save the plot in graphics/
+ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
 
